d521e9b3ef154cc7f9c66cb39faab9c3c9e37c51
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-06-29 12:03:29 -0500

    Initial Laravel application

---
+++ app/Http/routes.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Appointment reminders - @yield('title')</title>
+    </head>
+    <body>
+        @section('sidebar')
+            This is the master sidebar.
+        @show
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
