a3fd5a253a664dc48061f616d48348d82a93af04
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-07-20 11:28:06 -0500

    Switch to ten minutes schedule to match Heroku's

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('reminders:send')->everyMinute();
+        $schedule->command('reminders:send')->everyTenMinutes();
     }
 }
