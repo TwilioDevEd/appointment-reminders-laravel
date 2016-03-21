9fa04758a43d153a825a23f899526e5e0ca69233
Mario Celi <mceli@stackbuilders.com>
2016-03-21 14:58:59 -0500

    Update dependencies

--- config/database.php
+++ config/database.php
@@ -1,11 +1,19 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
+$host = '';
+$username = '';
+$password = '';
+$database = '';
 
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+$databaseUrl = getenv("DATABASE_URL");
+if ($databaseUrl) {
+  $url = parse_url(getenv("DATABASE_URL"));
+  $host = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $database = substr($url["path"], 1);
+}
+
 
 return [
 
@@ -61,10 +69,10 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', $host),
-            'database'  => env('DB_DATABASE', $database),
-            'username'  => env('DB_USERNAME', $username),
-            'password'  => env('DB_PASSWORD', $password),
+            'host'     => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
@@ -73,10 +81,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', $host),
+            'database'  => env('DB_DATABASE', $database),
+            'username'  => env('DB_USERNAME', $username),
+            'password'  => env('DB_PASSWORD', $password),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
